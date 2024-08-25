f4b1da0e07685d0188227e99c5ccd2ce463f991f
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-08-25 22:58:07 +0200

    svelte >>>>>>>

---
+++ src/components/Card.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+interface PropsComponent {
+    title: string;
+    description: string;
+    href: string;
+    thumbnail: string;
+    techno : Array<{img : string, name : string}>;
+}
+
+export let title: PropsComponent['title'];
+export let description: PropsComponent['description'];
+export let href: PropsComponent['href'];
+export let thumbnail: PropsComponent['thumbnail'];
+export let techno :PropsComponent['techno'];
+
+</script>
+
+<a href={href}>
+	<img src={thumbnail} alt="">
+	<h2>{title}</h2>
+	<p>{description}</p>
+	{#each techno as currentTechno}
+		<p>{currentTechno.img}</p>
+		<h1>{currentTechno.name}</h1>
+    {/each}
+</a >
