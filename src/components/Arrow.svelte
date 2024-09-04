88bac6afddd8241d7f2591dcd40b436179594d18
math-pixel <mathcb74@gmail.com>
2024-09-04 14:39:05 +0200

    add arrow and wip on css

---
+++ src/components/Arrow.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+</script>
+
+<LottiePlayer
+  src='/lotties/arrow.json'
+  autoplay={true}
+  loop={true}
+  controls={false}
+  renderer='svg'
+  background='transparent'
+  height={500}
+  width={500}
+    />
+  <!-- controlsLayout={[
+		"previousFrame",
+		"playpause",
+		"stop",
+		"nextFrame",
+		"progress",
+		"frame",
+		"loop",
+		"spacer",
+		"background",
+		"snapshot",
+		"zoom",
+		"info"
+	]}  -->
