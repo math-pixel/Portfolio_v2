ddfeedfa984efc62ad87ee4d8f2ebad05d32ed9e
math-pixel <mathcb74@gmail.com>
2024-09-04 16:12:37 +0200

    wip card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -16,9 +16,9 @@ export let techno :PropsComponent['techno'];
 
 </script>
 
-<a href={href}>
-	<img src={thumbnail} alt="">
-	<h2>{title}</h2>
+<a href={href} class=" absolute w-[40%] bg-red-400">
+	<img src={thumbnail} alt="" class=" absolute bottom-0 left-0 w-1/3 h-[120%]">
+	<h2 class="">{title}</h2>
 	<p>{description}</p>
 	{#each techno as currentTechno}
 		<p>{currentTechno.img}</p>
