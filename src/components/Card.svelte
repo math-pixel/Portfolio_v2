1d8c05de0a5feaaa078b7354de8c524a5a2a10d6
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-09-18 16:59:10 +0200

    update card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -16,18 +16,26 @@ export let techno :PropsComponent['techno'];
 
 </script>
 
-<a href={href} class="relative w-[40%] h-[40%] bg-[#D6ECFF] overflow-visible rounded-[20px]">
+<a href={href} class="relative  w-[45%] bg-[#D6ECFF] overflow-visible rounded-[20px] p-4 pt-7 m-2">
     
-    <img src={thumbnail} alt="" class=" absolute bottom-[12px] left-[12px] h-[105%] max-w-[35%] rounded-[10px]">
+    <div class=" flex w-full h-[45vh] overflow-hidden justify-around">
+        <img src={thumbnail} alt="" class=" h-[100%] w-[85%] rounded-[10px] object-cover ">
 
-    <h2 class="absolute top-[10%] left-[40%] font-semibold text-4xl">{title}</h2>
-    <p class="absolute top-[25%] left-[40%] font-normal text-lg ">{description}</p>
-
-    <div class=" absolute -bottom-[5%] right-[5%] w-[40%] h-[20%] flex justify-around items-center overflow-visible">
-        {#each techno as currentTechno, index}
-            {#if index < 3}
-                <img alt="of techno" src={currentTechno.img} class="w-[25%]"/>
-            {/if}
-        {/each}
+        <div class=" w-[10%] h-[90%] flex flex-col justify-start items-center overflow-visible">
+            {#each techno as currentTechno, index}
+                {#if index < 4}
+                    <img alt=" {currentTechno.img}of techno" src={currentTechno.img} class="w-[100%] rounded-[10px] mb-4"/>
+                {/if}
+            {/each}
+        </div>
     </div>
+
+    <div class=" flex flex-col w-full overflow-hidden max-w-[90%] m-[5%] ml-[1%]">
+
+        <h2 class=" top-[10%] left-[40%] font-semibold text-4xl">{title}</h2>
+        <p class=" top-[25%] left-[40%] font-normal text-lg ">{description}</p>
+    </div>
+
+
+
 </a >
