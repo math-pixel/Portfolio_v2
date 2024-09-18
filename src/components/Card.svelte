c62f9e743dbb13eed485dcc7696e5935ab47c18a
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-09-18 18:03:57 +0200

    add content in card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -16,21 +16,21 @@ export let techno :PropsComponent['techno'];
 
 </script>
 
-<a href={href} class="relative  w-[45%] bg-[#D6ECFF] overflow-visible rounded-[20px] p-4 pt-7 m-2">
+<a href={href} class="relative  w-[45%] overflow-visible rounded-[20px] p-4 pt-7 m-2 hover:bg-slate-100 ">
     
     <div class=" flex w-full h-[45vh] overflow-hidden justify-around">
-        <img src={thumbnail} alt="" class=" h-[100%] w-[85%] rounded-[10px] object-cover ">
+        <img src={thumbnail} alt="" class=" h-[100%] w-[100%] rounded-[10px] object-cover ">
 
-        <div class=" w-[10%] h-[90%] flex flex-col justify-start items-center overflow-visible">
+        <!-- <div class=" w-[10%] h-[90%] flex flex-col justify-start items-center overflow-visible">
             {#each techno as currentTechno, index}
                 {#if index < 4}
                     <img alt=" {currentTechno.img}of techno" src={currentTechno.img} class="w-[100%] rounded-[10px] mb-4"/>
                 {/if}
             {/each}
-        </div>
+        </div> -->
     </div>
 
-    <div class=" flex flex-col w-full overflow-hidden max-w-[90%] m-[5%] ml-[1%]">
+    <div class=" flex flex-col w-full overflow-hidden max-w-[90%] m-[3%] ml-[1%]">
 
         <h2 class=" top-[10%] left-[40%] font-semibold text-4xl">{title}</h2>
         <p class=" top-[25%] left-[40%] font-normal text-lg ">{description}</p>
