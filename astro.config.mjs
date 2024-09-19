4941c2cf133b375baee7272802327015f27c5caf
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-09-19 10:43:41 +0200

    update header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx()]
+  integrations: [tailwind(), svelte(), mdx(), alpinejs()]
 });
